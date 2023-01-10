06c00c31a3549562aa0392d37c46db99a081f9ee
SignalMover <timhollobon@gmail.com>
2023-01-11 10:41:11 +1300

    more

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parent New healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Hard-Boiled Free-Range Egg")
