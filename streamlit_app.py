58ffb166d03197e0a7f3e46a959060bf64420f88
SignalMover <timhollobon@gmail.com>
2023-01-11 10:51:05 +1300

    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title("My Parent New healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
