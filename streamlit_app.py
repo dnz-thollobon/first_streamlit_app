5c5ea364f39f26a3ec56956ca6969cb7107186f2
SignalMover <timhollobon@gmail.com>
2023-01-12 15:24:26 +1300

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,6 @@ except URLError as e:
     strealit.error
 
 
-streamlit.stop()
-
-
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
@@ -60,6 +57,11 @@ if streamlit.button('Get Fruit Load List'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         my_data_rows = get_fruit_load_list()
         streamlit.dataframe(my_data_rows)
+
+
+streamlit.stop()
+
+
 
 #allow user to add fruit
 add_my_fruit  = streamlit.text_input('What fruit would you like to add?','Jackfruit')
